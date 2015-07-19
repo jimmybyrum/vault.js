70c8ff51601516417efeeb0ac6a7255725fc7cfa
James Byrum <me@jimmybyrum.com>
2015-07-19 04:00:01 -0700

    another guess

--- test/parse.js
+++ test/parse.js
@@ -5,9 +5,29 @@ var assert = require('assert');
 
 describe('parse', function() {
 
-  before(function(next) {
+  describe('string', function () {
     Vault.set('name', 'jimmy');
+    var name = Vault.get('name');
+    it('should be a typeof string', function () {
+      assert.equal('string', typeof name);
+    });
+    it('should return a string', function () {
+      assert.equal('jimmy', name);
+    });
+  });
+
+  describe('number', function () {
     Vault.set('age', 33);
+    var age = Vault.get('age');
+    it('should be a typeof number', function () {
+      assert.equal('number', typeof age);
+    });
+    it('should return a number', function () {
+      assert.equal(33, age);
+    });
+  });
+
+  describe('array', function () {
     Vault.set('cities', [
       'Pittsburgh',
       'San Jose',
@@ -16,10 +36,36 @@ describe('parse', function() {
       'Cambridge',
       'San Francisco'
     ]);
+    var cities = Vault.get('cities');
+    it('should be a typeof object', function () {
+      assert.equal('object', typeof cities);
+    });
+    it('should have array length', function () {
+      assert.equal(6, cities.length);
+    });
+    it('should return an array item', function () {
+      assert.equal('San Francisco', cities[5]);
+    });
+  });
+
+  describe('object', function () {
     Vault.set('libs', {
       'vault.js': 'https://github.com/jimmybyrum/vault.js',
       'voice-commands.js': 'https://github.com/jimmybyrum/voice-commands.js'
     });
+    var libs = Vault.get('libs');
+    it('should be a typeof object', function () {
+      assert.equal('object', typeof libs);
+    });
+    it('should return the key', function () {
+      assert.equal('vault.js', Object.keys(libs)[0]);
+    });
+    it('should return the value', function () {
+      assert.equal('https://github.com/jimmybyrum/vault.js', libs['vault.js']);
+    });
+  });
+
+  describe('deep object', function () {
     Vault.set('deep', {
       name: 'James',
       age: 33,
@@ -35,58 +81,6 @@ describe('parse', function() {
         }
       }
     });
-    setTimeout(function() {
-      next();
-    }, 1000);
-  });
-
-  describe('string', function () {
-    var name = Vault.get('name');
-    it('should be a typeof string', function () {
-      assert.equal('string', typeof name);
-    });
-    it('should return a string', function () {
-      assert.equal('jimmy', name);
-    });
-  });
-
-  describe('number', function () {
-    var age = Vault.get('age');
-    it('should be a typeof number', function () {
-      assert.equal('number', typeof age);
-    });
-    it('should return a number', function () {
-      assert.equal(33, age);
-    });
-  });
-
-  describe('array', function () {
-    var cities = Vault.get('cities');
-    it('should be a typeof object', function () {
-      assert.equal('object', typeof cities);
-    });
-    it('should have array length', function () {
-      assert.equal(6, cities.length);
-    });
-    it('should return an array item', function () {
-      assert.equal('San Francisco', cities[5]);
-    });
-  });
-
-  describe('object', function () {
-    var libs = Vault.get('libs');
-    it('should be a typeof object', function () {
-      assert.equal('object', typeof libs);
-    });
-    it('should return the key', function () {
-      assert.equal('vault.js', Object.keys(libs)[0]);
-    });
-    it('should return the value', function () {
-      assert.equal('https://github.com/jimmybyrum/vault.js', libs['vault.js']);
-    });
-  });
-
-  describe('deep object', function () {
     var deep = Vault.get('deep');
     it('should be a typeof object', function () {
       assert.equal('object', typeof deep);
