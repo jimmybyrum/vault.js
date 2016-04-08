277e151a3a7b6bd44afdf82eb8e1613ee420359a
James Byrum <me@jimmybyrum.com>
2016-04-07 20:28:15 -0700

    fixing test

--- browser.js
+++ browser.js
@@ -3,12 +3,14 @@ var version = require('./package.json').version;
 var Cookie = require('./lib/browser').Cookie;
 var Local = require('./lib/browser').Local;
 var Session = require('./lib/browser').Session;
+var Memory = require('./lib/memory');
 
 module.exports = {
   version: version,
   Cookie: Cookie,
   Local: Local,
   Session: Session,
+  Memory: Memory,
   set: function(key, value, config) {
     if (config && config.expires) {
       return Local.set(key, value, config);
@@ -18,41 +20,36 @@ module.exports = {
     return Cookie.set(key, value, config);
   },
   get: function(key) {
-    var sess = Session.get(key);
-    if (sess !== undefined) {
-      return sess;
-    } else {
-      var local = Local.get(key);
-      if (local !== undefined) {
-        return local;
-      } else {
-        return Cookie.get(key);
-      }
-    }
+    return Memory.get(key) || Session.get(key) || Local.get(key) || Cookie.get(key);
   },
   list: function(raw) {
-    console.log('--== Local ==--');
-    Local.list(raw);
+    console.log('--== Memory ==--');
+    Memory.list(raw);
     console.log('--== Session ==--');
     Session.list(raw);
+    console.log('--== Local ==--');
+    Local.list(raw);
     console.log('--== Cookie ==--');
     Cookie.list(raw);
   },
   getLists: function() {
     return {
-      Local: Local.getList(),
+      Memory: Memory.getList(),
       Session: Session.getList(),
+      Local: Local.getList(),
       Cookie: Cookie.getList()
     };
   },
   remove: function(key) {
-    Local.remove(key);
+    Memory.remove(key);
     Session.remove(key);
+    Local.remove(key);
     Cookie.remove(key);
   },
   clear: function() {
-    Local.clear();
+    Memory.clear();
     Session.clear();
+    Local.clear();
     Cookie.clear();
   }
 };
--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 'use strict';
+
+var version = require('./package.json').version;
+
 if (typeof window !== 'undefined') {
   module.exports = require('./browser');
 } else {
-  module.exports = require('./lib/file');
+  var conf = require('./lib/config');
+  var path = require('path');
+  var appDir = path.dirname(require.main.filename);
+  var _file = appDir + conf.vaultFile;
+  var File = require('./lib/file');
+  console.log('Vault (' + version + ') File:', _file);
+  module.exports = {
+    version: version,
+    File: File.init(_file),
+    Memory: require('./lib/memory')
+  };
 }
