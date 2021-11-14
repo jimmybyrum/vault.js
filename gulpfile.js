7f4f0ebc645b2a78aee80d14b03611f3c8296a06
James Byrum <me@jimmybyrum.com>
2021-11-14 22:04:52 +0100

    updating gulp file and fixing browserify-uglify

--- gulpfile.js
+++ gulpfile.js
@@ -3,32 +3,43 @@ import clean from 'gulp-clean';
 import eslint from 'gulp-eslint';
 import rename from 'gulp-rename';
 import uglify from 'gulp-uglify';
-import browserify from 'gulp-browserify';
+import browserify from 'browserify';
+import source from 'vinyl-source-stream';
+import buffer from 'vinyl-buffer';
 
 let fail = false;
 
-gulp.task('clean', () => {
+gulp.task('clean', cleanTask);
+gulp.task('js', jsTask);
+gulp.task('eslint', eslintTask);
+gulp.task('watch', watchTask);
+
+gulp.task('build', cb => {
+  gulp.series('clean', 'js')();
+  cb();
+});
+
+function cleanTask(cb) {
   gulp.src('./dist/*')
     .pipe(clean({
       read: false
     }));
-});
+  cb();
+}
 
-gulp.task('js', () => {
-  gulp.src('./browser.js')
-    .pipe(browserify({
-      read: false,
-      standalone: 'Vault',
-      debug: process.env.NODE_ENV === 'dev'
-    }))
+function jsTask(cb) {
+  browserify(['browser.js'])
+    .transform('babelify')
+    .bundle()
+    .pipe(source('browser.js'))
+    .pipe(buffer())
     .pipe(uglify())
     .pipe(rename('vault.min.js'))
     .pipe(gulp.dest('./dist'));
-});
+  cb();
+}
 
-// gulp.task('build', ['clean', 'js'], () => {});
-
-gulp.task('eslint', () => {
+function eslintTask() {
   return gulp.src([
     './*.js',
     'lib/*.js'
@@ -41,13 +52,13 @@ gulp.task('eslint', () => {
       }
     }
   );
-});
+}
 
-gulp.task('watch', () => {
+function watchTask() {
   gulp.watch([
     './lib/*.js'
   ], ['js']);
-});
+}
 
 process.on('uncaughtException', function(err) {
   console.log(err);
